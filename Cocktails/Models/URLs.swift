3923b2f61b01038b7e9045fcdc8f9dd5891fa5b7
Vladimir Li <tekmya@mail.ru>
2022-05-04 23:14:46 +0300

    Теперь в секции по нажатию передается полностью все коктейли с названием, начинающемся с буквы из секции

--- Cocktails/Models/URLs.swift
+++ Cocktails/Models/URLs.swift
@@ -9,7 +9,6 @@ import Foundation
 
 enum URLs: String {
     case randomTenCocktails = "https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php"
-
     case firstAddress = "https://www.thecocktaildb.com/api/json/v2/9973533/recent.php"
-    
+    case allCocktails = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f="
 }
